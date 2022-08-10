09de5a90948ab912c2ab33e3cc0db50a43911ef8
Adael Cohen <cohenadael@gmail.com>
2022-08-10 12:24:25 -0400

    scaffold reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_160720) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_162300) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "adress"
     t.string "category"
     t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
